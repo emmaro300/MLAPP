cd815657b4799798cb05ad7b3429f40a7c3b5ef6
Arthur <emmaro300@gmail.com>
2022-11-07 04:16:39 -0800

    Web App Version 2

--- app.R
+++ app.R
@@ -4,154 +4,119 @@
 library(shiny)
 library(bslib)
 library(rsconnect)
-#Global variables
-prep <- c("Scale", "Normalise","Standardize", "Encode","Reduce Dimension")
-samp <- c("Random Sample", "Stratified Sample")
-resamp <- c("K-Fold CV", "Bootstrap")
-mod <- c("Ordinary Least Squares","Partial Least Squares","Principal Component Regression","Regularised Regression","Logistic Regression","Multi Adaptive Regression Spline","K Nearest Neighbour","Decision Tree","Bootstrap Aggregation","Random Forest","Gradient Boosting Machines","Extreme Gradient Boosting Machines", "Deep Learning Neural Network", "Support Vector Machines")
-regress <- c("MSE","RMSE","MAE","RSQ")
-classify <- c("Accuracy","Specificity","Sensitivity","Cross Entropy","Gini Index","ROC")
-plt <- c("ROC","VIP","PDP","CART","ANN","BEST_FIT")
+library(shinydashboard)
+
 # Define UI for Machine Learning application 
-ui <- fluidPage(
-  #Page title
-  theme = bs_theme(bootswatch="cerulean"),
-  title = "Machine Learning Models",
- 
-    # Application title
-    titlePanel("ML App"),
-    # Define navbarpage panel which contains tabpanel
-   navbarPage(
-     title = "ML",
-    tabPanel(
-      title = "Input Data",
-      fileInput(
-        "file",
-        "Upload data file",
-        buttonLabel = "Upload",
-        accept = ".csv",
-        placeholder = "csv only"
-      )
-      
+
+  ui <- dashboardPage(
+    dashboardHeader(title = "MLAPP"),
+    
+    sidebar = dashboardSidebar(
+      sidebarMenu(
+        menuItem(
+        "File", 
+        tabName = "file",
+          menuSubItem(text = "New", tabName = "new"),
+          menuSubItem(text = "Open", tabName = "open"),
+          menuSubItem(text = "Save As", tabName = "save_as"),
+          menuSubItem(text = "Save", tabName = "save"),
+          menuSubItem(text = "Exit", tabName = "save"),
+        icon = icon("th")
+        ),
+        menuItem(
+          "Preprocess", 
+          tabName = "preprocess",
+          menuSubItem(text = "Normalize", tabName = "normalize"),
+          menuSubItem(text = "Standardize", tabName = "standardize"),
+          menuSubItem(text = "Scale", tabName = "scale"),
+          menuSubItem(text = "Encode", tabName = "encode"),
+          menuSubItem(text = "Reduce Dimension",tabName = "rdim"),
+          icon = icon("list-alt")
     ),
-        
-    navbarMenu(
-      "Preprocess",
-    tabPanel(
-      "Normalize"
+       menuItem(
+        "Partition", 
+        tabName = "partition",
+         menuSubItem(text = "Random Sample", tabName = "rsamp"),
+         menuSubItem(text = "Stratifies Sample", tabName = "ssamp"),
+         icon = icon("list-alt")
     ),
-    tabPanel(
-      "Standardize"
+    menuItem(
+      "Resample", 
+      tabName = "resample",
+      menuSubItem(text = "K-Fold CV", tabName = "kcv"),
+      menuSubItem(text = "Bootstrap", tabName = "bootstrap"),
+      icon = icon("list-alt")
     ),
-    tabPanel(
-      "Scale"
+    menuItem(
+      "Train", 
+      tabName = "train",
+      menuSubItem(text = "Ordinary Least Squares", tabName = "ols"),
+      menuSubItem(text = "Partial Least Squares", tabName = "pls"),
+      menuSubItem(text = "Principal Component Regression", tabName = "pcr"),
+      menuSubItem(text = "Logistic Regression", tabName = "lr"),
+      menuSubItem(text = "Regularised Regression",tabName = "rr"),
+      menuSubItem(text = "Multi Adaptive Regression Spline",tabName = "mars"),
+      menuSubItem(text = "K Nearest Neighbour",tabName = "knn"),
+      menuSubItem(text = "Decision Tree",tabName = "dt"),
+      menuSubItem(text = "Bootstrap Aggregation",tabName = "bagging"),
+      menuSubItem(text = "Randome Forest",tabName = "rf"),
+      menuSubItem(text = "Gradient Boosting Machine",tabName = "gbm"),
+      menuSubItem(text = "Extreme Gradient Boosting Machine",tabName = "xgboost"),
+      menuSubItem(text = "Support Vector Machine",tabName = "svm"),
+      menuSubItem(text = "Deep Learning Neural Network",tabName = "dlnn"),
+      icon = icon("list-alt")
     ),
-    tabPanel(
-      "Encode"
+    menuItem(
+      "Evaluate", 
+      tabName = "evaluate",
+      menuSubItem(text = "MSE", tabName = "mse"),
+      menuSubItem(text = "RMSE", tabName = "rmse"),
+      menuSubItem(text = "MAE", tabName = "mae"),
+      menuSubItem(text = "RSQ", tabName = "rsq"),
+      menuSubItem(text = "Accuracy", tabName = "accuracy"),
+      menuSubItem(text = "Sensitivity", tabName = "sensitivity"),
+      menuSubItem(text = "Specificity", tabName = "specificity"),
+      menuSubItem(text = "ROC", tabName = "roc"),
+      menuSubItem(text = "Gini Index", tabName = "gi"),
+      menuSubItem(text = "Cross Entropy", tabName = "ce"),
+      icon = icon("list-alt")
     ),
-    tabPanel(
-      "Reduce Dimension"
+    menuItem(
+      "Test", 
+      tabName = "test",
+      menuSubItem(text = "Test", tabName = "test"),
+      menuSubItem(text = "Predict", tabName = "predict"),
+      icon = icon("list-alt")
+    ),
+    menuItem(
+      "Result", 
+      tabName = "result",
+      menuSubItem(text = "Table", tabName = "table"),
+      menuSubItem(text = "VIP", tabName = "vip"),
+      menuSubItem(text = "PDP", tabName = "pdp"),
+      menuSubItem(text = "ROC", tabName = "rocp"),
+      menuSubItem(text = "ANN", tabName = "ann"),
+      menuSubItem(text = "Tree", tabName = "tree"),
+      icon = icon("list-alt")
+    ),
+    menuItem(
+      "Help", 
+      tabName = "help",
+      menuSubItem(text = "MLAPP Documentation", tabName = "mlapp"),
+      menuSubItem(text = "Tutorial", tabName = "tutorial"),
+      menuSubItem(text = "Shortcut Summary", tabName = "ssummary"),
+      icon = icon("list-alt")
     )
-    ),
-    
-    navbarMenu(
-      "Partition",
-      tabPanel(
-        "Random Sample"
-      ),
-      tabPanel(
-        "Stratified Sample"
-      )
-    ),
-    
-    navbarMenu(
-      "Resample",
-      tabPanel(
-        "K-fold CV"
-      ),
-      tabPanel(
-        "Bootstrap"
-      )
-    ),
-    
-    navbarMenu(
-      "Train",
-      tabPanel(
-        "Ordinary Least squares"
-      ),
-      tabPanel(
-        "Partial Least Squares"
-      ),
-      tabPanel(
-        "Principal Component Regression"
-      ),
-      tabPanel(
-        "Logistic Regression"
-      ),
-      tabPanel(
-        "Regularized Regression"
-      ),
-      
-      tabPanel(
-        "Multi Adaptive Regression Spline"
-      ),
-      tabPanel(
-        "K Nearest Neighbour"
-      ),
-      tabPanel(
-        "Decision Tree"
-      ),
-      tabPanel(
-        "Bootstrap Aggregation"
-      ),
-      tabPanel(
-        "Random Forest"
-      ),
-      tabPanel(
-        "Gradient Boosting Machine"
-      ),
-      tabPanel(
-        "Extreme Gradient Boosting Machine"
-      ),
-      tabPanel(
-        "Deep Learning Neural Network"
-      ),
-      tabPanel(
-        "Support vector Machine"
-      )
-    ),
-    navbarMenu(
-      "Evaluate",
-      tabPanel(
-        "Regression"
-      ),
-      tabPanel(
-        "Classification"
-      )
-    ),
-      
-    navbarMenu(
-      "Test and Predict",
-      tabPanel(
-        "Test"
-      ),
-      tabPanel(
-        "Predict"
-      )
-    ),
-    navbarMenu(
-      "Results",
-      tabPanel(
-        "Table"
-      ),
-      tabPanel(
-        "Plot"
+  )
+),
+  
+    body = dashboardBody(
+      tabItems(
+        tabItem("hi")
       )
     )
-      
-   )
- )
-    
+  )
+
 # Define server logic required to run ML model
 server <- function(input, output) {
   
